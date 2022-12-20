9bf95b1d6b515966395da9e622bf13abf6dff8a3
canyvzdb <mcanyvz1996@gmail.com>
2022-12-20 19:35:01 +0300

    changes

---
+++ app.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+const colors = [
+    "black",
+    "yellow",
+    "green",
+    "#c7bf67",
+    "#7b55cf",
+    "#de4747",
+    "#2963ab",
+  ];
