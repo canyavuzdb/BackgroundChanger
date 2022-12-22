24c5514516c308456bb23dd46f28357c2f2359bf
canyvzdb <mcanyvz1996@gmail.com>
2022-12-22 08:53:19 +0300

    end

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const body = document.querySelector("body");
 const button = document.querySelector("button");
 const colors = [
-    "black",
-    "yellow",
-    "green",
-    "#c7bf67",
-    "#7b55cf",
-    "#de4747",
-    "#2963ab",
-  ];
+"black",
+"yellow",
+"green",
+"red",
+"purple",
+"orange",
+"brown",
+"blue",
+
+];
+
+button.addEventListener("click", changeBackgrpund);
+
+let order = 0;
+function changeBackgrpund(){
+  // random color 
+  // const randomNum = Math.floor(Math.random() * colors.length);
+  // const choosenColor = colors[randomNum];
+  // body.style.backgroundColor = choosenColor;
+  // with order
+  console.log(order);
+  if(order === colors.length) order = 0;
+  const choosenColor = colors[order];
+  order++;
+  body.style.backgroundColor = choosenColor;
+}
